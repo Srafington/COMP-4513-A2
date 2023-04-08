b4928d322dfe157a5574e4eae32c57819ff24db6
Jonathan Axford <sidewinder@live.ca>
2023-04-08 11:04:19 -0600

    Massaging files for Glitch

--- w3a2-server/mongoConnector.js
+++ w3a2-server/mongoConnector.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 //Connection setup
 const connect = (dbName) => {
@@ -7,7 +8,7 @@ const connect = (dbName) => {
       useNewUrlParser: true,
       dbName: dbName
    };
-   mongoose.connect(`mongodb+srv://${process.env.mongo_user}:${process.env.mongo_password}@${process.env.mongo_url}/${process.env.mongo_cluster}`, opt);
+   mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_URL}/${process.env.MONGO_CLUSTER}`, opt);
    const db = mongoose.connection;
    db.on('error', console.error.bind(console, 'connection error:'));
    db.once('open', function callback() {
